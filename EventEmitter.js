ad15e4a20d15a4e72444380facb95271f8b0fde8
kostiantyn <kostiantyn@test.com>
2023-10-08 19:35:31 -0230

    +EventEmitter, +ee

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,8 +1,8 @@
-const EventEmitter = require("note:events");
+const EventEmitter = require("node:events");
 
 class MyEmitter extends EventEmitter {
-  logInConsole() {
-    this.emit("logInConsole");
+  logInConsole(method, url, code, msg) {
+    this.emit("logInConsole", method, url, code, msg);
   }
 
   saveLog() {
@@ -11,5 +11,11 @@ class MyEmitter extends EventEmitter {
 }
 
 const ee = new MyEmitter();
+
+ee.on("logInConsole", (method, url, code, msg) => {
+  console.log(
+    `method: ${method}, requested url: ${url}, status code: ${code}, status message: ${msg}`
+  );
+});
 
 module.exports = ee;
