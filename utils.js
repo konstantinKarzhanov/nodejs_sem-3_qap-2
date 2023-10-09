884be969519bec8333a78387e954fbc28db4e35d
kostiantyn <kostiantyn@test.com>
2023-10-09 18:42:03 -0230

    renamed errorMsg function

--- utils.js
+++ utils.js
@@ -2,17 +2,17 @@ const { readFile } = require("node:fs/promises");
 const { join, sep } = require("node:path");
 
 const logEE = require("./emitter");
-const { errorMsg } = require("./logEvent");
+const { errorMessage } = require("./logEvent");
 
 exports.fetchFile = async (...args) => {
   const path = args.join(sep);
 
   try {
-    const data = await readFile(join(__dirnae, path));
+    const data = await readFile(join(__dirname, path));
     logEE.logFile("fetchFile", "sysInfo", "File fetched successfully");
     return data;
   } catch ({ name, message }) {
     logEE.logFile("fetchFile", "error", `${name}: ${message}`);
-    console.log(errorMsg(name, message));
+    console.log(errorMessage(name, message));
   }
 };
