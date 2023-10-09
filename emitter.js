235a0741875a3ce84e929e31f793ce811520c897
kostiantyn <kostiantyn@test.com>
2023-10-09 18:38:09 -0230

    -logConsole
    it doesn't make sense since we already have console.log for this matter

--- emitter.js
+++ emitter.js
@@ -4,27 +4,12 @@ const chalk = require("chalk");
 const { createLog, saveLog } = require("./logEvent");
 
 class LogEmitter extends EventEmitter {
-  logConsole(method, url, code, msg) {
-    const chMethod = chalk.cyan(method);
-    const chUrl = chalk.green(url);
-    const chCode = chalk.magenta(code);
-    const chMsg = chalk.yellow(msg);
-
-    this.emit("logConsole", chMethod, chUrl, chCode, chMsg);
-  }
-
   logFile(event, level, message) {
     this.emit("logFile", event, level, message);
   }
 }
 
 const logEE = new LogEmitter();
-
-logEE.on("logConsole", (method, url, code, msg) => {
-  console.log(
-    `method: ${method}, requested url: ${url}, status code: ${code}, status message: ${msg}`
-  );
-});
 
 logEE.on("logFile", (event, level, message) => {
   const logRootDir = "logs";
